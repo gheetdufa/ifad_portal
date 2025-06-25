0edd6abe10fbf21df740ec625dd859233fe425c0
Your Name <your.email@example.com>
2025-06-25 05:40:02 -0400

    added config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ifad-portal/', // Replace 'ifad-portal' with your actual repository name
+  base: 'https://github.com/gheetdufa/ifad_portal', // Replace 'your-repo-name' with your actual GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
