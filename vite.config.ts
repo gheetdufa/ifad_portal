b19131a7f55b85ee6af7c69d8a44de9502289473
Dheer <dheer@example.com>
2025-07-28 14:42:19 -0400

    update website with local changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ifad_portal/', // Use GitHub Pages path
+  base: '/', // Set to root for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     outDir: 'docs', // Output to docs for GitHub Pages
+    assetsDir: 'assets',
   },
 });
